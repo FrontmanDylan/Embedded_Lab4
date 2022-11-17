7fe80c52dc7a0b4f3aec72051fa23ad5eeabeba7
Frontman Dylan <98844974+FrontmanDylan@users.noreply.github.com>
2022-11-17 17:08:42 -0500

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,19 @@
+//Q1 - MSP430 watch dog timer is used to alternate green LED2 every 250ms
+
+#include <msp430.h>
+int main(void)
+{
+    WDTCTL = WDT_ADLY_250;     // WDT 1000ms, SMCLK, interval timer
+    P6OUT &= ~BIT6;
+    P6DIR |= BIT6;
+    PM5CTL0 &= ~LOCKLPM5;
+    SFRIE1 |= WDTIE;
+    _enable_interrupts();
+    while (1);
+    }
+
+    #pragma vector=WDT_VECTOR
+    __interrupt void wdtled(void)
+    {
+            P6OUT^=BIT6;
+    }
